04d44b0398159f0b7ba32ac1ac4103b59beff442
Eugene Che <indexpro24@gmail.com>
2023-10-28 19:51:24 +0300

    c

--- src/app/Models/Tag.php
+++ src/app/Models/Tag.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
 
 class Tag extends Model
 {
@@ -13,8 +16,13 @@ class Tag extends Model
 
     protected $table = 'tag';
 
-    public function taggable()
+    public function product(): MorphToMany
     {
         return $this->morphedByMany(Product::class, 'taggable')->withTimestamps();
     }
+
+    // public function parentable(): MorphToMany
+    // {
+    //     return $this->morphToMany();
+    // }
 }
