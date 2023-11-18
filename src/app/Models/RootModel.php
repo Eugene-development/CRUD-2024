26e172f4078aecf30328c1e8cfdcca6e9d944907
Eugene Che <indexpro24@gmail.com>
2023-11-18 18:32:35 +0300

    c

--- src/app/Models/RootModel.php
+++ src/app/Models/RootModel.php
@@ -8,11 +8,13 @@ use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RootModel extends Model
 {
     use HasFactory;
     use HasUuids;
+    use SoftDeletes;
 
     protected $guarded = [];
 
